0dc16634ba3c9c126940ce9b4c3a026535d8ead3
chihaso <cycyccq@gmail.com>
2025-04-13 02:18:18 +0000

    略歴を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Profile from './component/Profile'
+import Biography from './component/Biography'
 import PersonalDevelopments from './component/PersonalDevelopments'
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center mb-20">
       <section className="mt-20">
         <Profile />
+      </section>
+      <section className="mt-20">
+        <Biography />
       </section>
       <section className="mt-20">
         <PersonalDevelopments />

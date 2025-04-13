de357d132d5f454ab28e12a9a164d74da1386ec0
chihaso <cycyccq@gmail.com>
2025-04-13 01:46:27 +0000

    ポートフォリオサイト追加、審議法案ウォッチャーのウォッチャーの公開停止記載

--- app/component/Works.tsx
+++ app/component/Works.tsx
@@ -4,6 +4,16 @@ export default function Works() {
   return (
     <>
       <Work
+        title={'ポートフォリオサイト(このサイト)'}
+        imagePath={'/works/portfolio.png'}
+        description={
+          'このサイトです'
+        }
+        siteUrl={'https://dj3qx3fn5ma55.cloudfront.net/'}
+        githubUrl={'https://github.com/chihaso/portfolio'}
+        technologies={['Next', 'CloudFront', 'GitHubActions']}
+      />
+      <Work
         title={'書き散らし'}
         imagePath={'/works/scribblings.png'}
         description={
@@ -14,12 +24,12 @@ export default function Works() {
         technologies={['Next', 'CloudFront', 'GitHubActions']}
       />
       <Work
-        title={'審議法案ウォッチャー'}
+        title={'審議法案ウォッチャー（公開停止済み）'}
         imagePath={'/works/bill_watcher.png'}
         description={
           '国会で審議されている法案を一覧で表示したり、コメントしたりできます。また、ウォッチ中の法案の審議状況が更新されるとメールで通知します。法案データは衆議院のウェブサイトから１日１回取得しています。'
         }
-        siteUrl={'https://bill-watcher.com/'}
+        siteUrl={'#'}
         githubUrl={'https://github.com/chihaso/bill_watcher'}
         technologies={['Rails', 'CircleCI']}
       />
